b1b98c9bdfb433f20452b95932d3c1cab657983d
Slava <110696118+sl7one@users.noreply.github.com>
2023-04-29 00:39:09 +0300

    Rendering projects from an array of data.

---
+++ src/assets/projectList/projectList.js
@@ -0,0 +1,46 @@
+export const projectsList = [
+   {
+      livePageLink: 'https://sl7one.github.io/loco-it-react/',
+      gitHubLink: 'https://sl7one.github.io/loco-it-react/',
+      title: 'LocoIt',
+      description: `"Loco IT" is a showcase of a React-powered web application, highlighting the potential of this cutting-edge technology. From dynamic user interfaces to seamless interactions, the website offers an immersive demonstration of React's capabilities for creating modern and responsive web applications. Own project. SPA landing. Experience with animation.`,
+      utils: 'HTML, SASS, React, Router-dom',
+   },
+   {
+      livePageLink: 'https://iuliia-sokol.github.io/Ice-Project/',
+      gitHubLink: 'https://github.com/iuliia-sokol/Ice-Project',
+      title: 'Ice',
+      description: `A website showcasing a project created using various technologies. Users can explore the interactive interface and learn about the features and functionality of this project, such as design, functions, and other visual elements. Responsive and adaptive layout. Team project. Role: developer, section products. Modal windows.`,
+      utils: 'HTML, SASS, Parcel',
+   },
+   {
+      livePageLink: 'https://sl7one.github.io/projectFilm/',
+      gitHubLink: 'https://github.com/sl7one/projectFilm',
+      title: 'Filmoteka',
+      description: `A website showcasing a collection of films from various genres, including action, drama, comedy, and more. Users can browse through the films and access detailed information about each movie, such as its synopsis, cast and crew, and release date. Team project. Role: team leader`,
+      utils: 'HTML, SASS, JS, Notiflix, LightBox, Tui Pagination, Axios, REST API, AJAX, Parcel',
+   },
+
+   {
+      livePageLink: 'https://sl7one.github.io/react-pet-coronavirus-statistics/',
+      gitHubLink: 'https://github.com/sl7one/react-pet-coronavirus-statistics',
+      title: 'CoronaVirus-Stats',
+      description: `Own pet-project. Simple API, coronavirus stast`,
+      utils: 'React, Styled-Components, Axios, React-modal, Store',
+   },
+
+   {
+      livePageLink: 'https://loki99911.github.io/Node.JS_Project_Front/',
+      gitHubLink: 'https://github.com/Loki99911/Node.JS_Project_Front',
+      title: 'SoYuammy',
+      description: `A frontend part of a project developed using Node.js, featuring an interactive interface for user interaction with various project components. It may include registration, authentication, database interaction, data display/input, and other project-specific functionalities. The website is designed with modern trends in web development, responsive design, and additional features such as animations and visual effects. Team project. Role: developer. Workflow: AddRecipe page, Motivated modals. Front-end - build on GitHub, Back-end - build on render.com`,
+      utils: 'MERN stack, Axios, Styled-Components, Mui, Swiper, Select, Toastify',
+   },
+   {
+      livePageLink: 'https://sl7one.github.io/lgran-resto/',
+      gitHubLink: 'https://github.com/sl7one/lgran-resto',
+      title: 'LGran-resto',
+      description: `Own pet-project. This landing page is about getting familiar with the GSAP library.`,
+      utils: 'React, GSAP, Styled-components, React-select',
+   },
+];
