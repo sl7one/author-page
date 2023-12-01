dbe149451351171a423e3f4831a1a9244673b53a
Slava <110696118+sl7one@users.noreply.github.com>
2023-12-01 09:47:04 +0200

    Update BackgroundVideo.jsx

--- src/components/BackgroundVideo/BackgroundVideo.jsx
+++ src/components/BackgroundVideo/BackgroundVideo.jsx
@@ -6,6 +6,7 @@ export const BackgroundVideo = ({ flag }) => {
    return (
       <BackgroundVideoBox flag={flag}>
          <video
+            playsinline
             autoPlay
             muted
             loop
